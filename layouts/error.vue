501f83bb81b0c5d5c606c2d81715526fe7d49150
i330z <izzazahmed134@gmail.com>
2021-05-15 13:30:06 +0530

    canonical tags fix

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="error-page text-center">
+    <h1> Page Not Found</h1>
+    <div class="mt-3">
+        <nuxt-link to="/">Return to HomePage</nuxt-link>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.error-page{
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
